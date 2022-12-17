a27550239f5ddc7d27b449c020af7e9173cfba59
Thien100498 <thienk83@gmail.com>
2022-12-17 19:51:29 +0700

    fix

--- wp-content/themes/vizeon/woocommerce/single-product/short-description.php
+++ wp-content/themes/vizeon/woocommerce/single-product/short-description.php
@@ -15,4 +15,8 @@ if ( ! $post->post_excerpt ) return;
 ?>
 <div itemprop="description">
 	<?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
+	<div class="d-flex">
+		<div class="font-weight-normal mr-2">Chia sẻ : </div>
+		<div class="d-flex"></div>
+	</div>
 </div>
