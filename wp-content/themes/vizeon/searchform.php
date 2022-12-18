b97eee4bf03396e147f8a7cfd0382b0fc3a4c54f
tongdv <hongminhbka@gmail.com>
2022-12-18 17:26:49 +0700

    fix search

--- wp-content/themes/vizeon/searchform.php
+++ wp-content/themes/vizeon/searchform.php
@@ -12,7 +12,7 @@
 ?>
 <form method="get" class="searchform gva-main-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="gva-search">
-		<input name="s" maxlength="40" class="form-control input-large input-search" type="text" size="20" placeholder="<?php echo esc_attr__('Search...', 'vizeon') ?>">
+		<input name="s" maxlength="40" class="form-control input-large input-search" type="text" size="20" placeholder="Tìm kiếm ...">
 		<span class="input-group-addon input-large btn-search">
 			<input type="submit" class="fa" value="&#xf002;" />
 		</span>
