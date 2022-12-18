fd20478c26f53023a78d74c092d05e5b9b30e19c
Thien100498 <thienk83@gmail.com>
2022-12-18 12:39:30 +0700

    f

--- wp-content/plugins/woocommerce/templates/single-product/product-attributes.php
+++ wp-content/plugins/woocommerce/templates/single-product/product-attributes.php
@@ -24,7 +24,7 @@ if (!$product_attributes) {
 	return;
 }
 ?>
-<table class="table table-striped">
+<table class="table table-striped" style="border-style: none !important">
 	<?php foreach ($product_attributes as $product_attribute_key => $product_attribute) : ?>
 		<tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr($product_attribute_key); ?>">
 			<th class="woocommerce-product-attributes-item__label" style="border-style: none !important"><?php echo wp_kses_post($product_attribute['label']); ?></th>
