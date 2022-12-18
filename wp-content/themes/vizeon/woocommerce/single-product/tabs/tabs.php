fbaca1fbd168bf0879ff63ebee3fa7d23cd899fa
Thien100498 <thienk83@gmail.com>
2022-12-18 11:06:34 +0700

    f

--- wp-content/themes/vizeon/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/vizeon/woocommerce/single-product/tabs/tabs.php
@@ -23,54 +23,10 @@ $tabs = apply_filters('woocommerce_product_tabs', array());
 $_count = 0;
 
 if (!empty($tabs)) : ?>
-	<!-- <div style="background-color: #2E3A5B; padding: 40px 0px ">
-		<div class="container" style="padding: 40px;">
-			<div style="font-family: 'Montserrat';
-				font-style: normal;
-				font-weight: 600;
-				font-size: 32px;
-				color: white;
-				line-height: 40px;">
-				Bạn đang tìm sản phẩm cho xe gì
-			</div>
-			<div style="
-				font-family: 'Montserrat';
-				font-style: normal;
-				font-weight: 400;
-				font-size: 16px;
-				line-height: 24px;
-				color: white;
-				padding: 20px 0px
-				">
-				Raijin có đủ sản phẩm cho tất cả các dòng xe
-			</div>
-			<div class="d-flex" style="justify-content: space-between;">
-				<button type="button" class="btn btn-lg" style="background-color: white; width: 262px ; height:auto; color: '#2E3A5B'">
-					<span style="color: #2E3A5B">
-						Honda
-					</span>
-				</button>
-				<button type="button" class="btn btn-lg" style="background-color: white; width: 262px ; height:auto; color:'#2E3A5B'">
-					<span style="color: #2E3A5B">
-						Yamaha
-					</span></button>
-				<button type="button" class="btn btn-lg" style="background-color: white; width: 262px ; height:auto; color:'#2E3A5B'">
-					<span style="color: #2E3A5B">
-						Piagio
-					</span></button>
-				<button type="button" class="btn btn-lg" style="background-color: white; width: 262px ; height:auto; color:'#2E3A5B'">
-					<span style="color: #2E3A5B">
-						Xe hãng khác
-					</span>
-				</button>
-			</div>
-		</div>
-
-	</div> -->
 
 	<div class="woocommerce-tabs clearfix tabs-left">
 		<div class="woocommerce-tabs-inner clear fix">
-			<div class="woocommerce-tab-product-nav">
+			<!-- <div class="woocommerce-tab-product-nav">
 				<ul class="woocommerce-tab-product-info nav nav-tabs default clear-list">
 					<?php foreach ($tabs as $key => $tab) : ?>
 						<li class="<?php echo esc_attr($key); ?>_tab<?php echo esc_attr(($_count == 1 ? ' active' : '')); ?>">
@@ -80,14 +36,17 @@ if (!empty($tabs)) : ?>
 					endforeach; ?>
 				</ul>
 			</div>
-			<?php $_count = 1; ?>
+			<?php $_count = 1; ?> -->
 			<div class="tab-content col-xs-12">
-				<?php foreach ($tabs as $key => $tab) : ?>
+				<div class="tab-pane<?php echo esc_attr(($_count == 0 ? ' active' : '')); ?>" id="tab-<?php echo esc_attr(1); ?>">
+					<?php call_user_func($tabs[1]['callback'], 1, $tabs[1]) ?>
+				</div>
+				<!-- <?php foreach ($tabs as $key => $tab) : ?>
 					<div class="tab-pane<?php echo esc_attr(($_count == 0 ? ' active' : '')); ?>" id="tab-<?php echo esc_attr($key); ?>">
 						<?php call_user_func($tab['callback'], $key, $tab) ?>
 					</div>
 				<?php $_count++;
-				endforeach; ?>
+						endforeach; ?> -->
 			</div>
 		</div>
 	</div>
