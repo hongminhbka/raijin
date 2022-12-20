73ab6200d0e5b96ec6873a40429f082e5e1d39e9
khaimp <khaihust98@gmail.com>
2022-12-20 22:54:48 +0700

    f

--- wp-content/themes/vizeon/woocommerce/loop/orderby.php
+++ wp-content/themes/vizeon/woocommerce/loop/orderby.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Show options for ordering
  *
@@ -7,27 +8,27 @@
  * @version     3.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 ?>
-<form class="woocommerce-ordering pull-right" method="get">
-	<select name="orderby" class="orderby">
-		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
-			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
+<form class="woocommerce-ordering pull-right" method="get" style="float: left !important;">
+	<select name="orderby" class="orderby" style="background-color: #222628;">
+		<?php foreach ($catalog_orderby_options as $id => $name) : ?>
+			<option style="color: #ffffff;" value="<?php echo esc_attr($id); ?>" <?php selected($orderby, $id); ?>><?php echo esc_html($name); ?></option>
 		<?php endforeach; ?>
 	</select>
 	<?php
-		// Keep query string vars intact
-		foreach ( $_GET as $key => $val ) {
-			if ( 'orderby' === $key || 'submit' === $key ) {
-				continue;
-			}
-			if ( is_array( $val ) ) {
-				foreach( $val as $innerVal ) {
-					echo '<input type="hidden" name="' . esc_attr( $key ) . '[]" value="' . esc_attr( $innerVal ) . '" />';
-				}
-			} else {
-				echo '<input type="hidden" name="' . esc_attr( $key ) . '" value="' . esc_attr( $val ) . '" />';
-			}
+	// Keep query string vars intact
+	foreach ($_GET as $key => $val) {
+		if ('orderby' === $key || 'submit' === $key) {
+			continue;
 		}
+		if (is_array($val)) {
+			foreach ($val as $innerVal) {
+				echo '<input type="hidden" name="' . esc_attr($key) . '[]" value="' . esc_attr($innerVal) . '" />';
+			}
+		} else {
+			echo '<input type="hidden" name="' . esc_attr($key) . '" value="' . esc_attr($val) . '" />';
+		}
+	}
 	?>
 </form>
