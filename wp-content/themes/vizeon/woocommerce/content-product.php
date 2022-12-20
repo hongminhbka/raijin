7cb0d2142868375289ad984e94322552ded51e27
khaimp <khaihust98@gmail.com>
2022-12-20 23:43:47 +0700

    f

--- wp-content/themes/vizeon/woocommerce/content-product.php
+++ wp-content/themes/vizeon/woocommerce/content-product.php
@@ -112,7 +112,7 @@ $classes[] = 'product-block product';
        */
       do_action('woocommerce_after_shop_loop_item_title');
       ?>
-      <div class="product-meta">
+      <div class="product-meta" style="position: relative">
          <div class="clearfix"></div>
          <h3 class="shop-loop-title" style="text-align: center;"><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h3>
 
@@ -154,6 +154,12 @@ $classes[] = 'product-block product';
          <div class="justify-center" style="border: 1px solid #596481; border-radius: 3px">
             <a class="d-block" href="<?php esc_url(the_permalink()); ?>" style="color: #596481; text-align: center;"><i aria-hidden="true"></i><span style="color: #596481;"> Xem chi tiết</span></a>
          </div>
+      </div>
+      <div style="background-color: #222628; position: absolute;
+         width: 100% vw;
+         height: 375px;
+         left: 0px;
+         top: 2324px;">
       </div>
    </div>
 </div>
