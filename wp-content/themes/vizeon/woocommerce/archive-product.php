8514d8c18ebdcc46e4f7aa5ef159aa4345cfdb4b
khaimp <khaihust98@gmail.com>
2022-12-20 23:31:35 +0700

    f

--- wp-content/themes/vizeon/woocommerce/archive-product.php
+++ wp-content/themes/vizeon/woocommerce/archive-product.php
@@ -200,7 +200,7 @@ $woo_display = vizeon_display_modes_value();
         </div>
       <?php endif ?>
     </div>
-    <div class="mt-3 mb-1" style="
+    <div class="mt-3 mb-3" style="
         font-family: 'Montserrat';
         font-style: normal;
         font-weight: 500;
