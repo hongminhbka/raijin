b85863dab0cac5b995037e585a7f95f3d768466c
khaimp <khaihust98@gmail.com>
2022-12-20 22:28:26 +0700

    f

--- wp-content/themes/vizeon/woocommerce/archive-product.php
+++ wp-content/themes/vizeon/woocommerce/archive-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Template for displaying product archives, including the main shop page which is a post type archive.
  *
@@ -9,149 +10,186 @@
  * @version     3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-	get_header();
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+get_header();
 
-  $page_id = vizeon_id();
+$page_id = vizeon_id();
 
-  $sidebar_layout_config = vizeon_get_option('woo_sidebar_config', '');
-  $left_sidebar = vizeon_get_option('woo_left_sidebar', '');
-  $right_sidebar = vizeon_get_option('woo_right_sidebar', '');
+$sidebar_layout_config = vizeon_get_option('woo_sidebar_config', '');
+$left_sidebar = vizeon_get_option('woo_left_sidebar', '');
+$right_sidebar = vizeon_get_option('woo_right_sidebar', '');
 
-  if(is_shop()){
-    $sidebar_layout_config = get_post_meta( $page_id, 'vizeon_sidebar_config', true);
-    $left_sidebar = get_post_meta($page_id, 'vizeon_left_sidebar', true);
-    $right_sidebar = get_post_meta($page_id, 'vizeon_right_sidebar', true);
-     if ($sidebar_layout_config == "") {
-      $sidebar_layout_config = vizeon_get_option('woo_sidebar_config', ''); 
-    }
-    if ($left_sidebar == "") {
-      $left_sidebar = vizeon_get_option('woo_left_sidebar', '');  
-    }
-    if ($right_sidebar == "") {
-      $right_sidebar = vizeon_get_option('woo_right_sidebar', ''); 
-    }
+if (is_shop()) {
+  $sidebar_layout_config = get_post_meta($page_id, 'vizeon_sidebar_config', true);
+  $left_sidebar = get_post_meta($page_id, 'vizeon_left_sidebar', true);
+  $right_sidebar = get_post_meta($page_id, 'vizeon_right_sidebar', true);
+  if ($sidebar_layout_config == "") {
+    $sidebar_layout_config = vizeon_get_option('woo_sidebar_config', '');
   }
-
-	$left_sidebar_config  = array('active' => false);
-  $right_sidebar_config = array('active' => false);
-  $main_content_config  = array('class' => 'col-lg-12 col-xs-12');
-
-  if(isset($_GET['sb']) && $_GET['sb']){
-    $sidebar_layout_config = $_GET['sb'];
+  if ($left_sidebar == "") {
+    $left_sidebar = vizeon_get_option('woo_left_sidebar', '');
   }
+  if ($right_sidebar == "") {
+    $right_sidebar = vizeon_get_option('woo_right_sidebar', '');
+  }
+}
 
-	$sidebar_config = vizeon_sidebar_global($sidebar_layout_config, $left_sidebar, $right_sidebar);
-   
-	extract($sidebar_config);
+$left_sidebar_config  = array('active' => false);
+$right_sidebar_config = array('active' => false);
+$main_content_config  = array('class' => 'col-lg-12 col-xs-12');
 
-  $woo_display = vizeon_display_modes_value();
- ?>
+if (isset($_GET['sb']) && $_GET['sb']) {
+  $sidebar_layout_config = $_GET['sb'];
+}
+
+$sidebar_config = vizeon_sidebar_global($sidebar_layout_config, $left_sidebar, $right_sidebar);
+
+extract($sidebar_config);
+
+$woo_display = vizeon_display_modes_value();
+?>
 
 <section id="wp-main-content" class="clearfix main-page">
-    <?php
-      /**
-       * woocommerce_before_main_content hook
-       *
-       * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-       * @hooked woocommerce_breadcrumb - 20
-       */
-      do_action( 'woocommerce_before_main_content' );
-    ?>
-    
-   <div class="container">	
-   	  <div class="main-page-content row">
-         <div class="content-page <?php echo esc_attr($main_content_config['class']); ?>">      
-           
-     			  <div id="wp-content" class="wp-content">	
+  <?php
+  /**
+   * woocommerce_before_main_content hook
+   *
+   * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+   * @hooked woocommerce_breadcrumb - 20
+   */
+  do_action('woocommerce_before_main_content');
+  ?>
 
-  						<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+  <div class="container">
+    <div class="main-page-content row">
+      <div class="content-page <?php echo esc_attr($main_content_config['class']); ?>">
 
-  							<h1 class="page-title hidden"><?php woocommerce_page_title(); ?></h1>
+        <div id="wp-content" class="wp-content">
 
-  						<?php endif; ?>
+          <?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
 
-  						<?php do_action( 'woocommerce_archive_description' ); ?>
-              <?php woocommerce_product_subcategories(); ?>  
-     						<?php if ( have_posts() ) : ?>
-                  <div class="shop-loop-container">
-                    <div class="gvawooaf-before-products layout-<?php echo esc_attr($woo_display) ?>">
-       								<div class="woocommerce-filter clearfix">
-                        <?php
-         									/**
-         									 * woocommerce_before_shop_loop hook
-         									 *
-         									 * @hooked woocommerce_result_count - 20
-         									 * @hooked woocommerce_catalog_ordering - 30
-         									 */
-         									do_action( 'woocommerce_before_shop_loop' );
-         								?>
-                      </div> 
+            <h1 class="page-title hidden"><?php woocommerce_page_title(); ?></h1>
 
-                      <?php do_action('vizeon_woocommerce_active_filter' );  ?>
-                        
-                      <?php woocommerce_product_loop_start(); ?>
-     										
-                      <?php while ( have_posts() ) : the_post(); ?>
+          <?php endif; ?>
 
-     									<?php wc_get_template_part( 'content', 'product' ); ?>
-
-     									<?php endwhile; // end of the loop. ?>
-     							      
-                      <?php woocommerce_product_loop_end(); ?>
-
-     							<?php
-     								/**
-     								 * woocommerce_after_shop_loop hook
-     								 *
-     								 * @hooked woocommerce_pagination - 10
-     								 */
-     								do_action( 'woocommerce_after_shop_loop' );
-     							?> 
-                  </div>
+          <?php do_action('woocommerce_archive_description'); ?>
+          <?php woocommerce_product_subcategories(); ?>
+          <?php if (have_posts()) : ?>
+            <div class="container">
+              <div style="
+                font-family: 'Montserrat';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 32px;
+                line-height: 40px;
+                color: #2E3A5B
+                ">
+                Bạn đang tìm sản phẩm cho xe gì
+              </div>
+              <div class="my-5" style="
+                font-family: 'Montserrat';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 24px;
+                color: #2E3A5B;
+                ">
+                Raijin có đủ sản phẩm cho tất cả các dòng xe
+              </div>
+              <div class="row">
+                <div class="col-xs-1	col-sm-2	col-md-3	col-lg-3">
+                  <button type="button" class="my-2btn btn-outline-primary">Honda</button>
                 </div>
-  						<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-  							<?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-  						<?php endif; ?>
-
-  				 </div>
-   				
-         </div>      
-
-         <!-- Left sidebar -->
-         <?php if($left_sidebar_config['active']): ?>
-         <div class="sidebar wp-sidebar sidebar-left <?php echo esc_attr($left_sidebar_config['class']); ?>">
-            <?php do_action( 'vizeon_before_sidebar' ); ?>
-            <div class="sidebar-inner">
-               <?php dynamic_sidebar($left_sidebar_config['name'] ); ?>
+                <div class="col-xs-1	col-sm-2	col-md-3	col-lg-3">
+                  <button type="button" class="my-2btn btn-outline-primary">Yamaha</button>
+                </div>
+                <div class="col-xs-1	col-sm-2	col-md-3	col-lg-3">
+                  <button type="button" class="my-2btn btn-outline-primary">Piaggio</button>
+                </div>
+                <div class="col-xs-1	col-sm-2	col-md-3	col-lg-3">
+                  <button type="button" class="my-2btn btn-outline-primary">Xe hãng khác</button>
+                </div>
+              </div>
             </div>
-            <?php do_action( 'vizeon_after_sidebar' ); ?>
-         </div>
-         <?php endif ?>
+            <div class="shop-loop-container">
+              <div class="gvawooaf-before-products layout-<?php echo esc_attr($woo_display) ?>">
+                <div class="woocommerce-filter clearfix">
+                  <?php
+                  /**
+                   * woocommerce_before_shop_loop hook
+                   *
+                   * @hooked woocommerce_result_count - 20
+                   * @hooked woocommerce_catalog_ordering - 30
+                   */
+                  do_action('woocommerce_before_shop_loop');
+                  ?>
+                </div>
 
-         <!-- Right Sidebar -->
-         <?php if($right_sidebar_config['active']): ?>
-         <div class="sidebar wp-sidebar sidebar-right <?php echo esc_attr($right_sidebar_config['class']); ?>">
-            <?php do_action( 'vizeon_before_sidebar' ); ?>
-               <div class="sidebar-inner">
-                  <?php dynamic_sidebar($right_sidebar_config['name'] ); ?>
-               </div>
-            <?php do_action( 'vizeon_after_sidebar' ); ?>
-         </div>
-         <?php endif ?>
-      </div>   
-   </div>
+                <?php do_action('vizeon_woocommerce_active_filter');  ?>
 
-    <?php
-      /**
-       * woocommerce_after_main_content hook
-       *
-       * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-       */
-      do_action( 'woocommerce_after_main_content' );
-   ?>
+                <?php woocommerce_product_loop_start(); ?>
+
+                <?php while (have_posts()) : the_post(); ?>
+
+                  <?php wc_get_template_part('content', 'product'); ?>
+
+                <?php endwhile; // end of the loop. 
+                ?>
+
+                <?php woocommerce_product_loop_end(); ?>
+
+                <?php
+                /**
+                 * woocommerce_after_shop_loop hook
+                 *
+                 * @hooked woocommerce_pagination - 10
+                 */
+                do_action('woocommerce_after_shop_loop');
+                ?>
+              </div>
+            </div>
+          <?php elseif (!woocommerce_product_subcategories(array('before' => woocommerce_product_loop_start(false), 'after' => woocommerce_product_loop_end(false)))) : ?>
+
+            <?php wc_get_template('loop/no-products-found.php'); ?>
+
+          <?php endif; ?>
+
+        </div>
+
+      </div>
+
+      <!-- Left sidebar -->
+      <?php if ($left_sidebar_config['active']) : ?>
+        <div class="sidebar wp-sidebar sidebar-left <?php echo esc_attr($left_sidebar_config['class']); ?>">
+          <?php do_action('vizeon_before_sidebar'); ?>
+          <div class="sidebar-inner">
+            <?php dynamic_sidebar($left_sidebar_config['name']); ?>
+          </div>
+          <?php do_action('vizeon_after_sidebar'); ?>
+        </div>
+      <?php endif ?>
+
+      <!-- Right Sidebar -->
+      <?php if ($right_sidebar_config['active']) : ?>
+        <div class="sidebar wp-sidebar sidebar-right <?php echo esc_attr($right_sidebar_config['class']); ?>">
+          <?php do_action('vizeon_before_sidebar'); ?>
+          <div class="sidebar-inner">
+            <?php dynamic_sidebar($right_sidebar_config['name']); ?>
+          </div>
+          <?php do_action('vizeon_after_sidebar'); ?>
+        </div>
+      <?php endif ?>
+    </div>
+  </div>
+
+  <?php
+  /**
+   * woocommerce_after_main_content hook
+   *
+   * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+   */
+  do_action('woocommerce_after_main_content');
+  ?>
 </section>
 
 <?php get_footer(); ?>
