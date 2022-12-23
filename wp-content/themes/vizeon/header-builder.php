1d70a896b21f4a7eaca17a6f53adde33c93d56a4
hongminhbka <hongminhbka@gmail.com>
2022-12-23 16:39:11 +0700

    add header

--- wp-content/themes/vizeon/header-builder.php
+++ wp-content/themes/vizeon/header-builder.php
@@ -25,6 +25,32 @@
   <meta name="MobileOptimized" content="320"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="profile" href="http://gmpg.org/xfn/11">
+  <script>
+    //Function checks if a given script is already loaded
+    function isScriptLoaded(src){
+        return document.querySelector('script[src="' + src + '"]') ? true : false;
+    }
+    
+    //When a reply link is clicked, check if reply-script is loaded. If not, load it and emulate the click
+    $('.comment-reply-link').click(function(){ 
+        if(!(isScriptLoaded("/wp-includes/js/comment-reply.min.js"))){
+            var script = document.createElement('script');
+            script.src = "/wp-includes/js/comment-reply.min.js"; 
+        script.onload = emRepClick($(this).attr('data-commentid'));        
+            document.head.appendChild(script);
+        } 
+    });
+    //Function waits 50 ms before it emulates a click on the relevant reply link now that the reply script is loaded
+    function emRepClick(comId) {
+    sleep(50).then(() => {
+    document.querySelectorAll('[data-commentid="'+comId+'"]')[0].dispatchEvent(new Event('click'));
+    });
+    }
+    //Function does nothing, for a given amount of time
+    function sleep (time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    }
+  </script>
   <?php wp_head(); ?>
 </head>
 
